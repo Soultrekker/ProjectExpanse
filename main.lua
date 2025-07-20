e29b876f1c0acf3467b764ecc5371fedfd9ed1c2
Soultrekker <jacobdavis311@gmail.com>
2025-07-20 12:43:04 -0400

    reworked turning

--- main.lua
+++ main.lua
@@ -13,6 +13,14 @@ function love.load()
         {x = screen_width, y = screen_height},-- bottom-right
         {x = 0, y = screen_height}            -- bottom-left
     }
+
+    backvert = {
+        {x = 0, y = 0},                       -- top-left
+        {x = screen_width, y = 0},            -- top-right
+        {x = screen_width, y = screen_height},-- bottom-right
+        {x = 0, y = screen_height}            -- bottom-left
+    }
+
     rightvert = {
         {x = 0, y = 0},                       -- top-left
         {x = screen_width, y = 0},            -- top-right
@@ -37,6 +45,7 @@ function love.load()
         {x = screen_width, y = screen_height},-- bottom-right
         {x = 0, y = screen_height}            -- bottom-left
     }
+    
     Rratio = 1
     Lratio = 1
     lower_limit = 0.3
@@ -60,6 +69,14 @@ function love.load()
 
     front_mesh:setTexture(wall1_image)
 
+    back_mesh = love.graphics.newMesh({
+        {0, 0, 0, 0},  -- top-left
+        {1, 0, 1, 0},  -- top-right
+        {1, 1, 1, 1},  -- bottom-right
+        {0, 1, 0, 1}   -- bottom-left
+    }, "fan")
+    back_mesh:setTexture(wall3_image)
+
     right_mesh = love.graphics.newMesh({
         {0, 0, 0, 0},  -- top-left
         {1, 0, 1, 0},  -- top-right
@@ -67,7 +84,6 @@ function love.load()
         {0, 1, 0, 1}   -- bottom-left
     }, "fan")
     right_mesh:setTexture(wall2_image)
-
 
 
     left_mesh = love.graphics.newMesh({
@@ -100,9 +116,12 @@ turnstate = "stopped"
 turn_progress = 0
 look_state = "N" -- can be 1 North, 2 East, 3 South, 4 West
 turn_duration = 1.0  -- how long the turn lasts (seconds)
-turn_speed = .002
+turn_speed = .005
 move_forward = false
 gamestate = "menu"
+base_scale = 1
+scale = base_scale
+back_opacity = 0
 
 function love.keypressed(key)
     if key == "escape" then
@@ -128,64 +147,29 @@ function love.keypressed(key)
     end
 end
 
-base_scale = 1
-scale = base_scale
 
 
 function love.update(dt)
+    local angle = 0;
+    local direction = (turnstate == "right") and 1 or -1 
+    local top_center_of_spin = {x = center_x, y = center_y - screen_height *.42}
+    local bottom_center_of_spin = {x = center_x, y = center_y + screen_height *.42}
 
     if turnstate ~= "stopped" then --NOT EQUAL STOPPED
-        local right_edge = frontvert[2].x
-        local left_edge = frontvert[1].x
+        turn_progress = turn_progress + turn_speed
+        back_opacity = turn_progress
 
-        local direction = (turnstate == "right") and 1 or -1
 
-        center_x = center_x + turn_speed*screen_width*direction
-        Lratio = Lratio + turn_speed*direction*(-1)
-        Rratio = Rratio + turn_speed*direction
-
-        -- Voffsets per side
-        if not turn_mirrored then
-            if turnstate == "right" then
-                Voffset_R = Voffset_R + turn_speed * 2.1
-            else
-                Voffset_L = Voffset_L + turn_speed * 2.1
-            end
-        else
-            if turnstate == "right" then
-                Voffset_L = Voffset_L - turn_speed * 2.1
-            else
-                Voffset_R = Voffset_R - turn_speed * 2.1
-            end
-        end
-
-        -- Mirror at halfway point (simulate room face swap)
-        if not turn_mirrored and (frontvert[1].x <= - screen_width/10 or frontvert[2].x >= screen_width *1.1)  then
-            center_x = math.abs(screen_width - center_x)
-            local temp = Lratio
-            Lratio = Rratio
-            Rratio = temp
-            if turnstate == "right" then
-                Voffset_L = Voffset_R
-                Voffset_R = 1
-            else
-                Voffset_R = Voffset_L
-                Voffset_L = 1
-            end
-
-            changeLookState(turnstate == "right" and "left" or "right") -- Change look state after turn completes
-            turn_mirrored = true
-        end
-
-        if turn_mirrored and (center_x  > screen_width / 2 - scale and center_x < screen_width / 2 + scale) then
-            center_x = screen_width / 2
-            Lratio = 1
-            Rratio = 1
-            Voffset_L = 1
-            Voffset_R = 1
-            turn_mirrored = false
+        if turn_progress >= 1 then
+            turn_progress = 0
+            back_opacity = 0
             turnstate = "stopped"
+            changeLookState((direction == 1) and "right" or "left")
         end
+
+        local offset = math.sin(turn_progress * math.pi / 2) * 100
+
+        angle = angle + direction * turn_progress * math.pi/2 
 
     elseif turnstate == "stopped" then
         if move_forward then
@@ -197,26 +181,31 @@ function love.update(dt)
         end
     end
 
-
-
-     local topleft =   {x = center_x - screen_width*.3052* Lratio* scale, y = center_y - screen_height*.29 * Lratio* scale *Voffset_L}  -- top-left
-     local bottomleft = {x = center_x - screen_width*.3052* Lratio* scale, y = center_y + screen_height*.232* Lratio* scale*Voffset_L}  -- bottom-left
-
-
-     local topright = {x = center_x + screen_width*.3052* Rratio* scale, y = center_y - screen_height*.29 * Rratio* scale* Voffset_R}  -- top-right
-     local bottomright = {x = center_x + screen_width*.3052* Rratio* scale, y = center_y + screen_height*.232* Rratio* scale *Voffset_R}  -- bottom-right
+    local istop = -1
+    local isbot = 1
     
+    local topleft = rotate_about(top_center_of_spin.x, top_center_of_spin.y, istop, .75*math.pi + angle)
+    local topright = rotate_about(top_center_of_spin.x, top_center_of_spin.y, istop, .25*math.pi + angle)
+    local outertopleft = rotate_about(top_center_of_spin.x, top_center_of_spin.y, istop,  1.25*math.pi + angle)
+    local outertopright = rotate_about(top_center_of_spin.x, top_center_of_spin.y, istop, 1.75* math.pi + angle)
+    local bottomleft = rotate_about(bottom_center_of_spin.x, bottom_center_of_spin.y, isbot, 1.25*math.pi - angle)
+    local bottomright = rotate_about(bottom_center_of_spin.x, bottom_center_of_spin.y, isbot, 1.75*math.pi - angle)
+    local outerbottomleft = rotate_about(bottom_center_of_spin.x, bottom_center_of_spin.y, isbot, .75*math.pi - angle)
+    local outerbottomright = rotate_about(bottom_center_of_spin.x, bottom_center_of_spin.y, isbot, .25*math.pi - angle)
 
-     local outertopleft = {x = 0, y = 0}
-     local outertopright = {x = screen_width, y = 0}
-     local outerbottomright = {x = screen_width, y = screen_height}
-     local outerbottomleft = {x = 0, y = screen_height}
 
     frontvert = {
         topleft,
         topright,  -- top-right
         bottomright,  -- bottom-right
         bottomleft,  -- bottom-left
+    }
+
+    backvert = {
+        outertopleft,  -- top-left
+        outertopright,  -- top-right of screen
+        outerbottomright,  -- bottom-right of screen
+        outerbottomleft,  -- bottom-left
     }
 
     rightvert = {
@@ -231,18 +220,24 @@ function love.update(dt)
         bottomleft,  -- bottom-right same as bottom-left of frontvert
         outerbottomleft,  -- bottom-left
     }
-    bottomvert = {
-        bottomleft,  -- top-left
-        bottomright,  -- top-right
-        outerbottomright,  -- bottom-right of screen
-        outerbottomleft,  -- bottom-left
+
+
+    local bottomlookScene = { 
+        ["N"] = {bottomleft, bottomright, outerbottomright, outerbottomleft},
+        ["E"] = {outerbottomleft, bottomleft, bottomright, outerbottomright},
+        ["S"] = {outerbottomright, outerbottomleft, bottomleft, bottomright},
+        ["W"] = {bottomright, outerbottomright, outerbottomleft, bottomleft}
     }
-    topvert = {
-        outertopleft,  -- top-left
-        outertopright,  -- top-right of screen
-        topright,  -- bottom-right same as top-right of frontvert
-        topleft,  -- bottom-left same as top-left of frontvert
+
+    local toplookScene = { 
+        ["N"] = {outertopleft, outertopright, topright, topleft},
+        ["E"] = {outertopright, topright, topleft, outertopleft},
+        ["S"] = {topright, topleft, outertopleft, outertopright},
+        ["W"] = {topleft, outertopleft, outertopright, topright}
     }
+
+    bottomvert = bottomlookScene[look_state]
+    topvert = toplookScene[look_state]
 
     updateTextureDirection()
 
@@ -264,15 +259,29 @@ function love.draw()
     plotPolygons()
     love.graphics.setColor(1, 1, 1, 1) -- NORAMAL
     love.graphics.draw(front_mesh)
+
+    if( turnstate == "left") then
+        love.graphics.setColor(1, 1, 1, 1- back_opacity) 
+    end
     love.graphics.draw(right_mesh)
+    love.graphics.setColor(1, 1, 1, 1) -- NORAMAL
+
+
+    if( turnstate == "right") then
+        love.graphics.setColor(1, 1, 1, 1- back_opacity) 
+    end
     love.graphics.draw(left_mesh)
+    love.graphics.setColor(1, 1, 1, 1) -- NORAMAL
+
+
     love.graphics.draw(bottom_mesh)
     love.graphics.draw(top_mesh)
+    love.graphics.setColor(1, 1, 1, back_opacity) 
+    love.graphics.draw(back_mesh)
 end
 
 function plotPolygons()
     local tpoints, bpoints = {}, {}
-
     
     for _, v in ipairs(topvert) do
         table.insert(tpoints, v.x)
@@ -292,11 +301,15 @@ function plotPolygons()
 end
 
 function selectwalls()
-    local fpoints, rpoints, lpoints = {}, {}, {}
+    local fpoints, bpoints, rpoints, lpoints = {}, {}, {}, {}
 
     for _, v in ipairs(frontvert) do
         table.insert(fpoints, v.x)
         table.insert(fpoints, v.y)
+    end
+    for _, v in ipairs(backvert) do
+        table.insert(bpoints, v.x)
+        table.insert(bpoints, v.y)
     end
     for _, v in ipairs(rightvert) do
         table.insert(rpoints, v.x)
@@ -314,13 +327,13 @@ function selectwalls()
         {10, 0.8, 0.4, 1.0} --W
     }
 
-    local frontcolor, rightcolor, leftcolor = {}, {}, {}, {}
+    local frontcolor, rightcolor, leftcolor, backcolor = {}, {}, {}, {}
 
     local switch = {
-        ["N"] = function()  leftcolor, frontcolor, rightcolor = wall[1], wall[2], wall[3] end,
-        ["E"] = function()  leftcolor, frontcolor, rightcolor = wall[2], wall[3], wall[4] end,
-        ["S"] = function()  leftcolor, frontcolor, rightcolor = wall[3], wall[4], wall[1] end,
-        ["W"] = function() leftcolor, frontcolor, rightcolor = wall[4], wall[1], wall[2] end
+        ["N"] = function()  leftcolor, frontcolor, rightcolor, backcolor = wall[1], wall[2], wall[3], wall[4] end,
+        ["E"] = function()  leftcolor, frontcolor, rightcolor, backcolor = wall[2], wall[3], wall[4], wall[1] end,
+        ["S"] = function()  leftcolor, frontcolor, rightcolor, backcolor = wall[3], wall[4], wall[1], wall[2] end,
+        ["W"] = function() leftcolor, frontcolor, rightcolor, backcolor = wall[4], wall[1], wall[2], wall[3] end
     }
 
     if switch[look_state] then
@@ -336,6 +349,9 @@ function selectwalls()
 
     love.graphics.setColor(leftcolor)
     love.graphics.polygon("fill", unpack(lpoints))
+
+    love.graphics.setColor(unpack(backcolor))
+    love.graphics.polygon("fill", unpack(bpoints))
 end
 
 function changeLookState(direction)
@@ -370,69 +386,41 @@ function updateTextureDirection()
         left_mesh:setTexture(wall4_image)
         front_mesh:setTexture(wall1_image)
         right_mesh:setTexture(wall2_image)
+        back_mesh:setTexture(wall3_image)
 
-        bottom_mesh:setVertex(1, bottomvert[1].x, bottomvert[1].y, 0,0)    
-        bottom_mesh:setVertex(2, bottomvert[2].x, bottomvert[2].y, 1,0)
-        bottom_mesh:setVertex(3, bottomvert[3].x, bottomvert[3].y, 1,1)
-        bottom_mesh:setVertex(4, bottomvert[4].x, bottomvert[4].y, 0,1)
-    
-        top_mesh:setVertex(1, topvert[1].x, topvert[1].y, 0,0)
-        top_mesh:setVertex(2, topvert[2].x, topvert[2].y, 1,0)
-        top_mesh:setVertex(3, topvert[3].x, topvert[3].y, 1,1)
-        top_mesh:setVertex(4, topvert[4].x, topvert[4].y, 0,1)
 
     elseif look_state == "E" then
 
         left_mesh:setTexture(wall1_image)
         front_mesh:setTexture(wall2_image)
         right_mesh:setTexture(wall3_image)
-
-        bottom_mesh:setVertex(1, bottomvert[1].x, bottomvert[1].y, 1,0)    
-        bottom_mesh:setVertex(2, bottomvert[2].x, bottomvert[2].y, 1,1)
-        bottom_mesh:setVertex(3, bottomvert[3].x, bottomvert[3].y, 0,1)
-        bottom_mesh:setVertex(4, bottomvert[4].x, bottomvert[4].y, 0,0)
-
-        top_mesh:setVertex(1, topvert[1].x, topvert[1].y, 0,1)
-        top_mesh:setVertex(2, topvert[2].x, topvert[2].y, 0,0)
-        top_mesh:setVertex(3, topvert[3].x, topvert[3].y, 1,0)
-        top_mesh:setVertex(4, topvert[4].x, topvert[4].y, 1,1)
-    
-
-
+        back_mesh:setTexture(wall4_image)
    
     elseif look_state == "S" then
         left_mesh:setTexture(wall2_image)
         front_mesh:setTexture(wall3_image)
         right_mesh:setTexture(wall4_image)
+        back_mesh:setTexture(wall1_image)
 
-        bottom_mesh:setVertex(1, bottomvert[1].x, bottomvert[1].y, 1,1)    
-        bottom_mesh:setVertex(2, bottomvert[2].x, bottomvert[2].y, 0,1)
-        bottom_mesh:setVertex(3, bottomvert[3].x, bottomvert[3].y, 0,0)
-        bottom_mesh:setVertex(4, bottomvert[4].x, bottomvert[4].y, 1,0)
-    
-        top_mesh:setVertex(1, topvert[1].x, topvert[1].y, 1,1)
-        top_mesh:setVertex(2, topvert[2].x, topvert[2].y, 0,1)
-        top_mesh:setVertex(3, topvert[3].x, topvert[3].y, 0,0)
-        top_mesh:setVertex(4, topvert[4].x, topvert[4].y, 1,0)
 
 
     elseif look_state == "W" then
         left_mesh:setTexture(wall3_image)
         front_mesh:setTexture(wall4_image)
         right_mesh:setTexture(wall1_image)
-
-        bottom_mesh:setVertex(1, bottomvert[1].x, bottomvert[1].y, 0,1)    
-        bottom_mesh:setVertex(2, bottomvert[2].x, bottomvert[2].y, 0,0)
-        bottom_mesh:setVertex(3, bottomvert[3].x, bottomvert[3].y, 1,0)
-        bottom_mesh:setVertex(4, bottomvert[4].x, bottomvert[4].y, 1,1)
-    
-        top_mesh:setVertex(1, topvert[1].x, topvert[1].y, 1,0)
-        top_mesh:setVertex(2, topvert[2].x, topvert[2].y, 1,1)
-        top_mesh:setVertex(3, topvert[3].x, topvert[3].y, 0,1)
-        top_mesh:setVertex(4, topvert[4].x, topvert[4].y, 0,0)
+        back_mesh:setTexture(wall2_image)
 
     end
 
+    bottom_mesh:setVertex(1, bottomvert[1].x, bottomvert[1].y, 0,0)    
+    bottom_mesh:setVertex(2, bottomvert[2].x, bottomvert[2].y, 1,0)
+    bottom_mesh:setVertex(3, bottomvert[3].x, bottomvert[3].y, 1,1)
+    bottom_mesh:setVertex(4, bottomvert[4].x, bottomvert[4].y, 0,1)
+
+    top_mesh:setVertex(1, topvert[1].x, topvert[1].y, 0,0)
+    top_mesh:setVertex(2, topvert[2].x, topvert[2].y, 1,0)
+    top_mesh:setVertex(3, topvert[3].x, topvert[3].y, 1,1)
+    top_mesh:setVertex(4, topvert[4].x, topvert[4].y, 0,1)
 
     front_mesh:setVertex(1, frontvert[1].x, frontvert[1].y, 0, 0)
     front_mesh:setVertex(2, frontvert[2].x, frontvert[2].y, 1, 0)
@@ -449,5 +437,11 @@ function updateTextureDirection()
     left_mesh:setVertex(3, leftvert[3].x, leftvert[3].y, 1, 1)
     left_mesh:setVertex(4, leftvert[4].x, leftvert[4].y, 0, 1)
 
+    back_mesh:setVertex(1, backvert[1].x, backvert[1].y, 1, 0)
+    back_mesh:setVertex(2, backvert[2].x, backvert[2].y, 0,0)
+    back_mesh:setVertex(3, backvert[3].x, backvert[3].y, 0,1)
+    back_mesh:setVertex(4, backvert[4].x, backvert[4].y, 1, 1)
+end
 
+function move_forward()
 end
--- source/tools.lua
+++ source/tools.lua
@@ -9,3 +9,20 @@ function multiply_table(tbl, factor)
   end
   return result
 end
+
+--a horizontal radius and b vertical radius
+function rotate_about(originx, originy, isbot, angle)
+  local a_max = screen_width *.72  -- widest at top
+  local a_min = screen_width * 0.3  -- narrowest at bottom
+  local b = screen_height * 0.125 -- vertical radius for inner
+
+  local vertical_factor = isbot*math.sin(angle)  -- top=1, bottom=-1
+  local a = a_min + (a_max - a_min) * (vertical_factor + 1) / 2
+
+  local s = math.sin(angle)
+  local c = math.cos(angle)
+
+    return {
+        x = originx + a*c,
+        y = originy + b*s}
+end
